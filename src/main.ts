c07e0d22a0a88d4abdf41467ee421f602e90b6df
cristopher saraos <cristopher.saraos@trebol-it.com>
2025-10-03 14:25:33 -0300

    Implementación de proveedores para el cliente HTTP y el enrutador en la aplicación Angular.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,19 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
 import { App } from './app/app';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(App, {
+  providers: [
+    provideHttpClient(withInterceptors([
+      (req, next) => {
+        req = req.clone({
+          withCredentials: true
+        });
+        return next(req);
+      }
+    ])),
+    provideRouter(routes)
+  ]
+});
